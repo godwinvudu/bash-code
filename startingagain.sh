fb99cc896810eb6d0ba68b48f7b97079a472c7dd
adonis <adoniscrowe@gmail.com>
2026-07-20 00:30:01 +0000

    still an issue

--- startingagain.sh
+++ startingagain.sh
@@ -2,7 +2,7 @@
 echo 'hello'
 read -p 'whats your name?' name
 echo "hello, $name"
-read -p "how old are you ?" age
-if [[age -gt 18]];then
+read -p 'how old are you ?' age
+if [$age -gt 18];then
 echo 'that old huh..'
 fi
