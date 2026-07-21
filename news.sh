8a07a9c5307f1fd658e72af6b5aa8f515945ee2a
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-21 11:50:26 +0000

    Fix input prompt for age in news.sh

--- news.sh
+++ news.sh
@@ -2,7 +2,7 @@
 echo "hello user"
 read -p "what's your name?" name
 echo "hope you're doing well $name"
-read -s -p "how old are you?" age 
+read -p "how old are you?" age 
 if [ $age -gt 30 ];then
    echo "you're that old?"
 else
