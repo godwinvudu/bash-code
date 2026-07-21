fe2dcc5293b33ac6f680ce622bc04ec21245b66c
Godwin Vudu <adoniscrowe@gmail.com>
2026-07-21 11:54:33 +0000

    Fix formatting in greeting message in news.sh

--- news.sh
+++ news.sh
@@ -1,7 +1,7 @@
 #!usr/bin/evn bash
 echo "hello user"
 read -p "what's your name?" name
-echo "hope you're doing well $name"
+echo "hope you're doing well,$name"
 read -p "how old are you?" age 
 if [ $age -gt 30 ];then
    echo "you're that old?"
